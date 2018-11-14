c53064e00bdc0201c6130b4a924c67855caeebca
liguangyuan <liguangyuan@lechebang.com>
2018-11-14 10:29:34 +0800

    eslint配置

--- src/Views/login/Components/login.js
+++ src/Views/login/Components/login.js
@@ -1,41 +1,42 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TouchableOpacity,Image} from 'react-native';
-import {TextInput} from '../../../Components'
+import { StyleSheet, View, TextInput} from 'react-native';
+// import {TextInput} from '../../../Components'
 
 
 export default class App extends Component{
     constructor(props, context) {
         super(props, context);
         this.state = {
-          width: 50,
-          height: 50, 
-      };
+            width: 50,
+            height: 50 
+        };
     }
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput placeholder='请输入手机号' />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput style={styles.inputStyle} 
+                    placeholder='请输入手机号' 
+                    underlineColorAndroid='transparent'
+                    placeholderTextColor={'#c1c1c1'}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color:'#333'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    example: {
+
+    },
+    inputStyle: {
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgb(77, 247, 43)'
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    }
 });
