85307ca1135e7eb56a1f28b426c76e219ed23e85
liguangyuan <liguangyuan@lechebang.com>
2018-12-10 14:57:40 +0800

    ios地图和定位显示

--- src/Views/discorver/Components/Discorver.js
+++ src/Views/discorver/Components/Discorver.js
@@ -1,24 +1,62 @@
-import React, {Component} from 'react';
-import { MapView } from 'react-native-baidumap-sdk';
-// import {View, Button} from 'react-native';
-// import actions from '../Action';
+import React, { Component } from 'react';
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { MapView, Location } from 'react-native-baidumap-sdk';
+import icon from '../../../Images/ic_my_location.png';
 
+const style = StyleSheet.create({
+    button: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        backgroundColor: '#fff',
+        borderRadius: 40,
+        elevation: 2
+    },
+    icon: {
+        width: 24,
+        height: 24,
+        margin: 12,
+        tintColor: '#616161'
+    }
+});
 
-export default class Discorver extends Component{
-    
-    componentDidMount() {
-        this.props.locationInit();
+export default class Discorver extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
-    render() {
-        console.log(this.props);
-        return (
-            <MapView
-                style={{ flex: 1 }}
-                center={{ latitude: 39, longitude: 113 }}
-                zoomLevel={10}
-            />
-        );
+
+    async componentDidMount() {
+        await Location.init();
+        Location.setOptions({ gps: true });
+        this.listener = Location.addLocationListener(location => {
+            this.setState({ location });
+        });
+        Location.start();
     }
+
+  location = () => this.mapView.setStatus({ center: this.state.location }, 500);
+
+  render() {
+      return (
+          <View style={StyleSheet.absoluteFill}>
+              <MapView
+                  ref={ref => this.mapView = ref}
+                  style={StyleSheet.absoluteFill}
+                  zoomLevel={18}
+                  location={this.state.location}
+                  locationEnabled
+                  zoomControlsDisabled
+              />
+              <View style={style.button}>
+                  <TouchableOpacity onPress={this.location}>
+                      <Image style={style.icon} source={icon} />
+                  </TouchableOpacity>
+              </View>
+          </View>
+      );
+  }
 }
 
 
